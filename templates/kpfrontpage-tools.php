c2f2ea005241d08a34457e4b3fd3ba8ca853203a
Reynold Tan <reynoldltan@gmail.com>
2019-09-09 12:03:34 -0600

    Updated tools section - additional instruction how to organize items

--- templates/kpfrontpage-tools.php
+++ templates/kpfrontpage-tools.php
@@ -14,10 +14,12 @@
 
   <ul style="display: none">
   ...
+  apply spacer to achieve balance and center fewer than 6 items.
   </ul>
 
   <ul style="display: none">
   ...
+  apply spacer to achieve balance and center fewer than 6 items.
   </ul>
 
   // NOTE:
@@ -28,7 +30,17 @@
   - Group tool items into six (6) item per UL. Ensure that succeding UL or list is set
   to hidden by default. An option to expose these hidden items is available in the frontend.
   @see note below.
+
+  - To maintain balance count and item positioning, apply a spacer element.
+  <li class="kpfrontpage-tools-spacer">&nbsp;</li>
+  to indicate a blank or empty item.
+  Exampple: 10 tools, 6 item first row and 4 items 2nd row.
+
+  tool 1 tool 2 tool 3 tool 4 tool 5 tool 6
+  spacer tools 7 tool 8 tool 9 tool 10 spacer - this will center 4 items underneath 6 item row.
   */
+
+  // ORDER (based on frequencey of use): BLAST, JBrowse, Genotype Matrix, CVITjs, VCF Bulk Export, Experiments
 
   ////////////
 ?>
@@ -49,18 +61,18 @@
                  </li>
 
                  <li>
+                   <div><img src="<?php echo $path_images . 'tool-icon/cvitjs.gif'; ?>" alt="CViTjs provides an interactive whole genome diagram and was developed by the Legume Federation." /></div>
+                   <a href="cvitjs">CVIT JS</a>
+                 </li>
+
+                 <li>
+                   <div><img src="<?php echo $path_images . 'tool-icon/bulk-loader.gif'; ?>" alt="Bioinformatics tool to filter an existing Variant Call Format (VCF) files and export into common formats such as ABH, Haplotype Map (Hapmap) or Bgzipped VCF." /></div>
+                   <a href="filter_vcf">VCF BULK EXPORT</a>
+                 </li>
+
+                 <li>
                    <div><img src="<?php echo $path_images . 'tool-icon/experiments.gif'; ?>" alt="Many projects produce multiple types of data which are made available as a dataset for easy lookup of project deliverables." /></div>
                    <a href="research/projects">EXPERIMENTS</a>
-                 </li>
-
-                 <li>
-                   <div><img src="<?php echo $path_images . 'tool-icon/variants.gif'; ?>" alt="Recent large scale genomics projects have resulted a large pool of defined sequence variation between germplasm." /></div>
-                   <a href="search/variants">VARIANTS</a>
-                 </li>
-
-                 <li>
-                   <div><img src="<?php echo $path_images . 'tool-icon/sequences.gif'; ?>" alt="KnowPulse provides to both genomic and transcript sequence from Chickpea, Common Bean, Field Pea and Lentil." /></div>
-                   <a href="search/sequences">SEQUENCES</a>
                  </li>
                </ul>
 
@@ -70,34 +82,28 @@
                ?>
 
                <ul style="display: none;">
+                 <li class="kpfrontpage-tools-spacer">&nbsp;</li>
+
                  <li>
-                   <div><img src="<?php echo $path_images . 'tool-icon/markers.gif'; ?>" alt="Many variants already have markers developed for them, providing a good resource for marker association studies." /></div>
-                   <a href="search/markers">MARKERS</a>
+                   <div><img src="<?php echo $path_images . 'tool-icon/trait-distribution.gif'; ?>" alt="Visualization tool to summarize the observed values for a specific trait in a particular phenotyping experiment." /></div>
+                   <a href="phenotypes/trait-distribution">TRAIT DISTRIBUTION PLOT</a>
                  </li>
 
                  <li>
-                   <div><img src="<?php echo $path_images . 'tool-icon/cvitjs.gif'; ?>" alt="" /></div>
-                   <a href="cvitjs">CVIT JS</a>
-                 </li>
-
-                 <li>
-                   <div><img src="<?php echo $path_images . 'tool-icon/germplasm-matrix.gif'; ?>" alt="" /></div>
+                   <div><img src="<?php echo $path_images . 'tool-icon/germplasm-matrix.gif'; ?>" alt="This module also provides a means for keeping track of RIL development through use of stock properties." /></div>
                    <a href="germplasm/summary/Lens">GERMPLASM MATRIX</a>
                  </li>
 
                  <li>
-                   <div><img src="<?php echo $path_images . 'tool-icon/user-charts.gif'; ?>" alt="" /></div>
+                   <div><img src="<?php echo $path_images . 'tool-icon/user-charts.gif'; ?>" alt="Visualization tool to generate Heatmap Chart, Beanplot Chart and Principal Complonent Analysis (PCoA)." /></div>
                    <a href="user_charts">USER CHARTS</a>
                  </li>
 
                  <li>
-                   <div><img src="<?php echo $path_images . 'tool-icon/position-convert.gif'; ?>" alt="" /></div>
+                   <div><img src="<?php echo $path_images . 'tool-icon/position-convert.gif'; ?>" alt="Positino Convert converts marker positions from L.culinaris genome version 0.8 to 1.2." /></div>
                    <a href="posconvert">POSITION CONVERT</a>
                  </li>
 
-                 <li>
-                   <div><img src="<?php echo $path_images . 'tool-icon/bulk-loader.gif'; ?>" alt="" /></div>
-                   <a href="filter_vcf">BULK LOADER</a>
-                 </li>
+                 <li class="kpfrontpage-tools-spacer">&nbsp;</li>
                </ul>
 
